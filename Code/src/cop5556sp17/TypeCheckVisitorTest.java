2bf7c52ee8ab1d18230dc34cf9ea0f17e7ab91e3
Venkat Sandeep <sandeep.kv92@gmail.com>
2017-03-18 19:30:30 -0400

    hw4 :: prep for TypeCheckVisitor

--- Code/src/cop5556sp17/TypeCheckVisitorTest.java
+++ Code/src/cop5556sp17/TypeCheckVisitorTest.java
@@ -22,6 +22,28 @@ public class TypeCheckVisitorTest {
     // TODO test case where program name is also declared as a variable
     // TODO Your Visitor class will both decorate the tree and check conditions along the way. If a condition is violated, throw a TypeCheckException.
 
+    // Program
+    // TODO Test complex programs i.e., random codes from previous test cases
+
+    // ParamDec
+    // TODO Test paramDec with multiple params (each one with different types). Assertion by using lookup into the symbol table
+    // TODO Negative - Test paramDec with multiple params of the same name. Should throw a TypeCheckException
+    // TODO Test a valid case with paramDec containing same name as the program name
+
+    // Block
+    // TODO Nothing to test here
+
+    // Dec
+    // TODO Test Dec with multiple params (each one with different types). Assertion by using lookup into the symbol table
+    // TODO Test Dec with multiple params of the same name. Should throw a TypeCheckException
+    // TODO Test a valid case with Dec containing same name as the program name
+
+    // Statement
+    // SleepStatement
+    // TODO positive and negative test case
+
+    // AssignmentStatement
+    // TODO positive and negative test case for each type
     @Test
     public void testAssignmentBoolLit0() throws Exception {
         String input = "p {\nboolean y \ny <- false;}";
@@ -45,5 +67,58 @@ public class TypeCheckVisitorTest {
         program.visit(v, null);
     }
 
+    // Chain
+    // ChainElem
+    // IdentChain
+    // TODO Negative - test case where ident has not been declared
+    // TODO Negative - test case where ident has been declared but not visible in this scope
+    // TODO - test case where ident is declared and visible. Write assertions for identChain.type and indent.type
+
+    // FilterOpChain
+    // TODO Negative - test case where tuple size is not zero
+    // TODO - test case where tuple size is zero. Write assertions for FilterOpChain.type
+
+    // FrameOpChain
+    // TODO positive and negative test cases for each of the if else branch
+
+    // ImageOpChain
+    // TODO positive and negative test cases for each of the if else branch
+
+    // BinaryChain
+    // TODO positive and negative test cases for each of the legal combination
+
+    // WhileStatement
+    // TODO positive and negative test cases. The expression in each of these test cases can be literals or idents/expression
+
+    // IfStatement
+    // TODO positive and negative test cases. The expression in each of these test cases can be literals or idents/expression
+
+    // Expression
+    // IdentExpression
+    // TODO Negative - test case where ident has not been declared
+    // TODO Negative - test case where ident has been declared but not visible in this scope
+    // TODO - test case where ident is declared and visible. Write assertions for identExpression.type and identExpression.dec
+
+    // IdentLValue
+    // TODO Negative - test case where ident has not been declared
+    // TODO Negative - test case where ident has been declared but not visible in this scope
+    // TODO - test case where ident is declared and visible. Write assertions for identLValue.dec
+
+    // IntLitExpression
+    // TODO write assertions to check the type. Can we write negative test cases here ?
+
+    // BooleanLitExpression
+    // TODO write assertions to check the type. Can we write negative test cases here ?
+
+    // ConstantExpression
+    // TODO write assertions to check the type. Can we write negative test cases here ?
+
+    // BinaryExpression
+    // TODO positive and negative test cases for each of the legal combination
+
+    // Tuple
+    // TODO Condition succeeds for empty tuple
+    // TODO Condition succeeds for non-empty tuple
+    // TODO Negative - Condition fails for list of expressions which have different types
 
 }
