d906e2004ad23cde3b8d64f6fae4bdcd3928bb43
Venkat Sandeep <sandeep.kv92@gmail.com>
2017-04-11 16:05:36 -0400

    hw6 :: Initial Commit

--- Code/src/cop5556sp17/CodeGenUtils.java
+++ Code/src/cop5556sp17/CodeGenUtils.java
@@ -1,21 +1,39 @@
 package cop5556sp17;
 
-/**
- * This class contains several static methods useful when developing
- * the code generation part of our compiler.
- */
-
 import cop5556sp17.AST.Type.TypeName;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.util.TraceClassVisitor;
 
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * This class contains several static methods useful when developing
+ * the code generation part of our compiler.
+ */
+
 public class CodeGenUtils {
+
+
+    /**
+     * Converts the provided classfile, generally created by asm,
+     * in a human readable format and returns as a String.
+     *
+     * @param bytecode
+     */
+    public static String bytecodeToString(byte[] bytecode) {
+        int flags = ClassReader.SKIP_DEBUG;
+        ClassReader cr;
+        cr = new ClassReader(bytecode);
+        StringWriter out = new StringWriter();
+        cr.accept(new TraceClassVisitor(new PrintWriter(out)), flags);
+        return out.toString();
+    }
 
     /**
      * Prints the provided classfile, generally created by asm,
@@ -27,14 +45,14 @@ public class CodeGenUtils {
         int flags = ClassReader.SKIP_DEBUG;
         ClassReader cr;
         cr = new ClassReader(bytecode);
-        cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), flags);
+        PrintStream out = System.out;
+        cr.accept(new TraceClassVisitor(new PrintWriter(out)), flags);
     }
 
     /**
      * Creates an instance of the indicated class from the provided byteCode.
      * args is passed as a parameter to the constructor, and in order to
      * be the correct type for generated code, should be String[]
-     *
      *
      * @param name
      * @param byteCode
@@ -69,18 +87,35 @@ public class CodeGenUtils {
      */
     public static void genPrint(boolean GEN, MethodVisitor mv, String message) {
         if (GEN) {
-            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+//      mv.visitFieldInsn(Opcodes.GETSTATIC, "cop5556sp17/PLPRuntimeLog", "globalLog", "Lcop5556sp17/PLPRuntimeLog;");
             mv.visitLdcInsn(message);
-            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "cop5556sp17/PLPRuntimeLog", "globalLogAddEntry", "(Ljava/lang/String;)V", false);
         }
     }
+
+//  /**
+//   * Generates code to print the given String.
+//   * IF !GEN, does not generate code.
+//   * Used to allow observation of execution of generated program
+//   * during development and grading.
+//   * 
+//   * @param mv
+//   * @param message
+//   */
+//  public static void genPrint(boolean GEN, MethodVisitor mv, String message) {
+//      if(GEN){
+//      mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+//      mv.visitLdcInsn(message);
+//      mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
+//      }
+//  }
 
     /**
      * Generates code to print the value on top of the stack without consuming it.
      * If !GEN, does not generate code.
-     *
+     * <p>
      * GEN Requires stack is not empty, and type matches the given type.
-     *
+     * <p>
      * Currently implemented only for integer and boolean.
      *
      * @param GEN
@@ -90,15 +125,21 @@ public class CodeGenUtils {
     public static void genPrintTOS(boolean GEN, MethodVisitor mv, TypeName type) {
         if (GEN) {
             mv.visitInsn(Opcodes.DUP);
-            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-            mv.visitInsn(Opcodes.SWAP);
+//          mv.visitFieldInsn(Opcodes.GETSTATIC, "cop5556sp17/PLPRuntimeLog", "globalLog", "Lcop5556sp17/PLPRuntimeLog;");
+//          mv.visitInsn(Opcodes.SWAP);
             switch (type) {
                 case INTEGER: {
-                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(I)V", false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Integer", "toString", "(I)Ljava/lang/String;", false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "cop5556sp17/PLPRuntimeLog", "globalLogAddEntry", "(Ljava/lang/String;)V", false);
                 }
                 break;
                 case BOOLEAN: {
-                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Z)V", false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Boolean", "toString", "(Z)Ljava/lang/String;", false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "cop5556sp17/PLPRuntimeLog", "globalLogAddEntry", "(Ljava/lang/String;)V", false);
+                }
+                break;
+                case IMAGE: {
+                    /* ignore */
                 }
                 break;
                 default: {
@@ -111,7 +152,6 @@ public class CodeGenUtils {
     /**
      * Loader for dynamically generated classes.
      * Instantiated by getInstance.
-     *
      */
     public static class DynamicClassLoader extends ClassLoader {
         public DynamicClassLoader(ClassLoader parent) {
